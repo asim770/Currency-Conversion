da7d9274ee48b6d910c481e75f0979cdd4326938
asim770 <majiasim.1234@gmail.com>
2025-05-17 12:47:11 +0530

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { InputBox } from './components'
+import {useCurrencyinfo} from './hooks'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount,setAmount]=useState(0)
+  const [currency,setCurrency]=useState("usd")
+  const [to,setTo] =useState("inr")
+  const [from,setFrom] = useState("usd")
+  const [convertedAmount,setConvertedAmount]=useState(0)
+  const currencyInfo=useCurrencyinfo(from)
 
   return (
     <>
